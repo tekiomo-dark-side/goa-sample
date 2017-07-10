23c507e26e332fbb62285a89b7a191f3572a81c4
ABE Tomonori <tekiomo@gmail.com>
2017-07-10 10:49:04 +0900

    first commit

---
+++ operands.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/goadesign/goa"
+	"github.com/tekiomo-dark-side/goa-sample/app"
+	"strconv"
+)
+
+// OperandsController implements the operands resource.
+type OperandsController struct {
+	*goa.Controller
+}
+
+// NewOperandsController creates a operands controller.
+func NewOperandsController(service *goa.Service) *OperandsController {
+	return &OperandsController{Controller: service.NewController("OperandsController")}
+}
+
+// Add runs the add action.
+func (c *OperandsController) Add(ctx *app.AddOperandsContext) error {
+	sum := ctx.Left + ctx.Right
+	return ctx.OK([]byte(strconv.Itoa(sum)))
+}
